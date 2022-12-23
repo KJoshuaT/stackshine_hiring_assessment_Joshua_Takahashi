9399d4a994ef3c4a01530cbc28ec871939a99496
Kazuma Takahashi <kjtakaha@ucsc.edu>
2022-12-23 09:59:42 -0800

    Implmented seed data to views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @workspaces = Workspace.all
+  end
+
+  def vendors
+    @workspaces = Workspace.all
+    @workspace_vendors = WorkspaceVendor.all
+    @vendor_transactions = VendorTransaction.all
   end
 
   def members
